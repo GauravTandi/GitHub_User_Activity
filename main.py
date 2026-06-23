14c393e819a1db1774c4e32f7b0fe9db91431a10
GauravTandi <148497273+GauravTandi@users.noreply.github.com>
2026-06-23 04:20:42 +0000

    call github api with urllib.request

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
+import urllib.request
 
 username = sys.argv[1]
 
-print(username)
+url =  f"https://api.github.com/users/{username}/events"
+
+response = urllib.request.urlopen(url)
+
+data = response.read()
+
+print(data)
